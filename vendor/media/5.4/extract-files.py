15a2a30132b2413e4ce4a7f4bde269204e8639d1
Jyotiraditya Panda <jyotiraditya@aospa.co>
2025-08-27 20:08:31 +0530

    common: Remove vendor.qti perf libraries
    - Switch to OSS/reversed interfaces
    
    Change-Id: I98263376ce5641176428ea32d704de343f2008ad
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: CuriousNom <aryanbiswas7585@gmail.com>

--- vendor/media/5.4/extract-files.py
+++ vendor/media/5.4/extract-files.py
@@ -5,11 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from extract_utils.fixups_blob import blob_fixup, blob_fixups_user_type
 from extract_utils.fixups_lib import lib_fixups, lib_fixups_user_type
 from extract_utils.main import ExtractUtils
 
-from extract_utils_qti.fixups_lib import lib_fixup_vendor_suffix
 from extract_utils_qti.module import ExtractUtilsQTIModule, QTIComponentType
 
 namespace_imports = [
@@ -22,21 +20,11 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'writepid\s+/dev/cpuset/foreground/tasks', 'task_profiles ProcessCapacityHigh HighPerformance'),
 }
 
-lib_fixups: lib_fixups_user_type = {
-    **lib_fixups,
-    (
-        'vendor.qti.hardware.perf@2.0',
-        'vendor.qti.hardware.perf@2.1',
-        'vendor.qti.hardware.perf@2.2',
-    ): lib_fixup_vendor_suffix,
-}
-
 module = ExtractUtilsQTIModule(
     'media/5.4',
     QTIComponentType.VENDOR,
     namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
-    lib_fixups=lib_fixups,
 )
 
 if __name__ == '__main__':
